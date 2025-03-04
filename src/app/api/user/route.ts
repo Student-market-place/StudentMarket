28eeef9d7695c3379cb6b89bf2d5ee801822f5b7
karpetoryba <karpenkokarpenko24@gmail.com>
2025-03-04 09:26:33 +0100

    new routes for api user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: "User " }, { status: 200 });
+}
+
+export async function POST(req: NextRequest) {
+  return NextResponse.json({ message: "User created" }, { status: 201 });
+}
