5cd85dc8a0d2a7ef25e09d911ffcb034c2636534
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2025-03-05 14:31:55 +0100

    feature/ create filterBlock + list student + list offers + logique buttons

--- src/app/(pages)/home/Page.tsx
+++ src/app/(pages)/home/Page.tsx
@@ -1,11 +1,76 @@
+"use client";
+import CardJobOffer from "@/components/custom-ui/CardOffer";
+import CardStudent from "@/components/custom-ui/CardStudent";
+import FilterBlock from "@/components/custom-ui/FilterBlock";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 const HomePage = () => {
+  const [activeTab, setActiveTab] = useState<"students" | "offers">("students");
+
+  const handleActive = (tab: "students" | "offers") => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div>
-      <h1>Hello home page</h1>
-      {/* 
-      - StudentsList
-      - OffersList
-      */}
+    <div className="flex flex-col gap-10 w-full">
+      {/* Boutons de sélection */}
+      <div className="flex justify-center gap-10">
+        <Button
+          className={`px-6 py-2 border-2 rounded-md transition-colors ${
+            activeTab === "students"
+              ? " bg-blue-500 text-white border-transparent hover:bg-blue-500"
+              : "bg-white text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white"
+          }`}
+          onClick={() => handleActive("students")}
+        >
+          Étudiants
+        </Button>
+        <Button
+          className={`px-6 py-2 border-2 rounded-md transition-colors ${
+            activeTab === "offers"
+              ? " bg-blue-500 text-white border-transparent hover:bg-blue-500"
+              : "bg-white text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white"
+          }`}
+          onClick={() => handleActive("offers")}
+        >
+          Offres
+        </Button>
+      </div>
+
+      {/* Contenu dynamique selon l'onglet actif */}
+      <div className="flex gap-8">
+        <FilterBlock />
+        <div className="grid grid-cols-4 gap-8">
+          {activeTab === "students" ? (
+            // Affichage des étudiants
+            <>
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+              <CardStudent />
+            </>
+          ) : (
+            <>
+              <CardJobOffer />
+              <CardJobOffer />
+              <CardJobOffer />
+              <CardJobOffer />
+              <CardJobOffer />
+              <CardJobOffer />
+              <CardJobOffer />
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
