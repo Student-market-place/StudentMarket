5ff93ba4db0f2a77484a1b11096193a87e70bc66
karpetoryba <karpenkokarpenko24@gmail.com>
2025-03-07 10:02:27 +0100

    new component StudentProfilCard

--- src/components/custom-ui/StudentProfilCard.tsx
+++ src/components/custom-ui/StudentProfilCard.tsx
@@ -13,31 +13,27 @@ import { Input } from "@/components/ui/input";
 
 export function StudentProfilCard() {
   return (
-    <Card className="w-[460px]  p-0 shadow-lg rounded-2xl overflow-hidden border-2 border-transparent  transition-all  ">
-      <CardHeader className="p-4 bg-gradient-to-r text-black">
-        <CardTitle className="text-lg font-bold">StudentProfilCard</CardTitle>
-        <CardDescription className="text-sm text-black opacity-90">
-          TechNovaX est une entreprise innovante spécialisée dans le
-          développement de solutions numériques sur mesure.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="p-0">
+    <Card className="relative flex bg-clip-border  bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row">
+      <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
         <img
           src="https://images.pexels.com/photos/4298629/pexels-photo-4298629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="Company"
-          className="w-full h-40 object-cover"
+          className="object-cover w-full h-full"
         />
-        <p className="p-4 text-black text-sm">
-          Nous offrons une gamme complète de services, allant de la conception
-          de sites web et d'applications mobiles à l'optimisation des stratégies
-          digitales et au marketing en ligne.
+      </div>
+      <CardHeader className="p-6 bg-gradient-to-r text-black">
+        <CardTitle className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
+          Maria Karpova
+        </CardTitle>
+        <CardDescription className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          Étudiante en développement à la recherche d'un stage
+        </CardDescription>
+        <p className="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+          Passionnée par la programmation et les nouvelles technologies, je
+          recherche un stage pour acquérir de l'expérience pratique et
+          approfondir mes compétences en développement logiciel.
         </p>
-      </CardContent>
-      <CardFooter className="p-4 flex justify-between items-center ">
-        <Button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-all">
-          En savoir plus
-        </Button>
-      </CardFooter>
+      </CardHeader>
     </Card>
   );
 }
