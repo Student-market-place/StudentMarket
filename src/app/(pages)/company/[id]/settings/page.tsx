1fa185c0074bb9286c3b61d650070a107c77af8a
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2025-03-10 11:27:51 +0100

    create buttons in company settings page

--- src/app/(pages)/company/[id]/settings/page.tsx
+++ src/app/(pages)/company/[id]/settings/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import CompanyProfilCard from "@/components/custom-ui/CompanyProfileUpdateCard";
+import { Button } from "@/components/ui/button";
 import CompanyService from "@/services/company.service";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 const SettingsCompanyPage = () => {
@@ -25,9 +27,18 @@ const SettingsCompanyPage = () => {
     return <div>No data found.</div>;
   }
   return (
-    <div>
+    <div className="flex flex-col p-8">
       <div className="flex justify-center items-center flex-col">
         <CompanyProfilCard key={company.id} company={company} />
+        <div className="flex gap-4 mt-4">
+          <Link href={`/company/${id}/update`}>
+            <Button>Modifier le profil</Button>
+          </Link>
+          <Link href={`/company/${id}/offers`}>
+            <Button>Mes offres</Button>
+          </Link>
+          <Button>Candidatures reçues</Button>
+        </div>
       </div>
     </div>
   );
