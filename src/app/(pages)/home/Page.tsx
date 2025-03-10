e07ba04d56b6c47b8eaca0cb2c9221fff16ab6a0
1u1ucas <lucasleveque17@gmail.com>
2025-03-10 11:49:01 +0100

    Ajout du contrôle d'accès dynamique sur la page d'accueil
    - Intégration de la récupération du profil utilisateur via localStorage
    - Conditionnement de l'affichage des onglets en fonction du rôle de l'utilisateur
    - Mise en place d'un useEffect pour charger les informations utilisateur au chargement
    - Ajout de logs de débogage pour le suivi du chargement utilisateur

--- src/app/(pages)/home/Page.tsx
+++ src/app/(pages)/home/Page.tsx
@@ -2,14 +2,27 @@
 
 import FilterBlock from "@/components/custom-ui/FilterBlock";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import UserService from "@/services/user.service";
+import { UserWithRelations } from "@/types/user.type";
 
 import StudentList from "@/components/custom-ui/StudentList";
 import OffersList from "@/components/custom-ui/OffersList";
 import CompanyList from "@/components/custom-ui/CompanyList";
 
 const HomePage = () => {
+  const [user, setUser] = useState<UserWithRelations | null>(null);
   const [activeTab, setActiveTab] = useState<"students" | "offers" | "companies">("students");
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      UserService.fetchUserById(userId)
+        .then(data => setUser(data))
+        .catch(err => console.error("Erreur lors du chargement de l'utilisateur:", err));
+    }
+    console.log("🔍 Utilisateur chargé:", user);
+  }, []);
 
   const handleActive = (tab: "students" | "offers" | "companies") => {
     setActiveTab(tab);
@@ -19,9 +32,10 @@ const HomePage = () => {
     <div className="flex flex-col gap-10 w-full">
       {/* Boutons de sélection */}
       <div className="flex justify-center gap-10">
-        <Button
-          className={`px-6 py-2 border-2 rounded-md transition-colors ${
-            activeTab === "students"
+        {user?.role === "company" || user?.role === "school" || user?.role === "admin" && (
+            <Button
+              className={`px-6 py-2 border-2 rounded-md transition-colors ${
+                activeTab === "students"
               ? " bg-blue-500 text-white border-transparent hover:bg-blue-500"
               : "bg-white text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white"
           }`}
@@ -29,6 +43,7 @@ const HomePage = () => {
         >
           Étudiants
         </Button>
+        )}
         <Button
           className={`px-6 py-2 border-2 rounded-md transition-colors ${
             activeTab === "offers"
@@ -39,6 +54,7 @@ const HomePage = () => {
         >
           Offres
         </Button>
+   
         <Button
           className={`px-6 py-2 border-2 rounded-md transition-colors ${
             activeTab === "companies"
