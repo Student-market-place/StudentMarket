4522d98aff207fef83b871c910fbe0b124922bf3
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2025-03-11 22:51:13 +0100

    create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,8 @@ export default function Home() {
               détaillés, où vous pouvez évaluer leurs compétences, leur
               expérience et leurs projets réalisés. Gérez les candidatures en
               toute simplicité, consultez les profils des étudiants qui
-              postulent et suivez facilement l&apos;évolution de vos recrutements.
+              postulent et suivez facilement l&apos;évolution de vos
+              recrutements.
               <br /> Student Market vous permet de gagner en efficacité et de
               trouver rapidement les talents qui feront la différence dans vos
               projets
@@ -129,10 +130,6 @@ export default function Home() {
             height={300}
           />
         </div>
-      </div>
-      <div className="flex justify-center gap-10">
-        <Link href="/privacy-policy">Politique de confidentialité</Link>
-        <Link href="/terms-of-use">Conditions d&apos;utilisations</Link>
       </div>
     </div>
   );
